85106ff55897d722817335e51cf75ef4d74a3d2f
RimJoudi <joudirim93@gmail.com>
2021-12-02 21:03:20 +0100

    fix

--- 0x00-ls/options_fn.c
+++ 0x00-ls/options_fn.c
@@ -20,22 +20,21 @@ void op_longformat(struct dirent *read)
 		_strncpy(time, ctime(&fileStat.st_mtime), 24);
 		lstat(read->d_name, &fileStat);
 
-			printf((S_ISDIR(fileStat.st_mode))  ? "d" : "-");
-			printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
-			printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
-			printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
-			printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
-			printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
-			printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
-			printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
-			printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
-			printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
+		printf((S_ISDIR(fileStat.st_mode))  ? "d" : "-");
+		printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+		printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+		printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+		printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+		printf((fileStat.st_mode & S_IXOTH) ? "x" : "-");
 
-			usr = getpwuid(fileStat.st_uid);
-			grp = getgrgid(fileStat.st_gid);
+		usr = getpwuid(fileStat.st_uid);
+		grp = getgrgid(fileStat.st_gid);
 
 		printf(" %lu %s %s %li %s %s\n", fileStat.st_nlink, usr->pw_name,
 		grp->gr_name, (intmax_t)fileStat.st_size, time, read->d_name);
 
 }
-
